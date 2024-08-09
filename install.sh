499b68ae043d08f8074c2871074485e897eeb505
Javed Iqbal <78470362+ijaved85@users.noreply.github.com>
2024-08-09 16:23:53 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git clone https://github.com/Cabbagec/termux-ohmyzsh
+cd termux-ohmyzsh && bash install.sh 
+clear
+cd ..
+
+bash banner.sh
---
+++ keyboard.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir $HOME/.termux/ ;
+clear
+echo "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]" >> $HOME/.termux/termux.properties && termux-reload-settings && sleep 1
